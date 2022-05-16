9c1270e2a2208435c960e1052549471dd7933e70
Ayush Sharam <ayush2123.be21@chitkara.edu.in>
2022-05-16 23:28:08 +0530

    hungry?

--- helloworld.cpp
+++ helloworld.cpp
@@ -10,8 +10,15 @@ if(n == 1){
     cout << "Eat Sanwich";
 }
 
+else if(n == 2){
+    cout << "Eat Pizza";
+}
+
 else{
     cout << "Drink Water";
 }
+cout << "Are you still hungry ? ";
+
+
 return 0;
 }
